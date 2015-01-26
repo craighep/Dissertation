83d1d1c91bf2aff5e5465ce00c03b140d9521f0d
Craig Heptinstall <crh13@aber.ac.uk>
2015-01-26 21:55:05 +0000

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,8 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    qunit: {
-      files: ['http://craighep.github.io/Dissertation/js/tests/*.html']
-    },
-  });
   grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.registerTask('test', ['qunit']);
+    gruntConfig.qunit = {
+        src: ['http://craighep.github.io/Dissertation/js/tests/tests.html']
+    };
+    grunt.registerTask('test', 'qunit:src');
+ 
 };
