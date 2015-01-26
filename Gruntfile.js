50e123c8d22ef6c82401353f2e3a9011e6fdb08f
Craig Heptinstall <crh13@aber.ac.uk>
2015-01-26 14:35:08 +0000

    Change grunt task to test

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,5 +6,5 @@ module.exports = function(grunt) {
     },
   });
   grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.registerTask('default', ['qunit']);
+  grunt.registerTask('test', ['qunit']);
 };
