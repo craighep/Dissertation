c1fd898517f69fe19bb8658b618ad4fd3bf9d542
Craig Heptinstall <crh13@aber.ac.uk>
2015-02-20 22:00:44 +0000

    Changes to about, test added

--- js/tests/tests.js
+++ js/tests/tests.js
@@ -1,3 +1,4 @@
-QUnit.test( "hello test", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
+QUnit.test( "Pause Animation", function( assert ) {
+  pause(false);
+  assert.ok( paused == false, "Passed!" );
 });
